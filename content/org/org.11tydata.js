b9fd36eac31f8a16e23c6b83bb00c3da5c9a2360
Lars Kellogg-Stedman <lars@oddbit.com>
2026-03-20 09:55:30 -0400

    All files for an org live in org subdirectory

--- content/org/org.11tydata.js
+++ content/org/org.11tydata.js
@@ -2,15 +2,15 @@ import { existsSync, readdirSync } from "fs";
 import { join, dirname } from "path";
 
 export default {
-  eleventyComputed: {
-    coverImage: (data) => {
-      const slug = data.page.fileSlug;
-      const coverDir = join(dirname(data.page.inputPath), slug);
-      if (!existsSync(coverDir)) return null;
-      const files = readdirSync(coverDir);
-      const cover = files.find((f) => /^cover\./i.test(f));
-      if (!cover) return null;
-      return `/org/${slug}/${cover}`;
-    },
-  },
+	eleventyComputed: {
+		coverImage: (data) => {
+			const slug = data.page.fileSlug;
+			const coverDir = dirname(data.page.inputPath);
+			if (!existsSync(coverDir)) return null;
+			const files = readdirSync(coverDir);
+			const cover = files.find((f) => /^cover\./i.test(f));
+			if (!cover) return null;
+			return `/org/${slug}/${cover}`;
+		},
+	},
 };
